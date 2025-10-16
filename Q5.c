18dbc3ab29bbb707d64251570ebf38834305e01f
Abdcoder-07 <k250868@nu.edu.pk>
2025-10-16 20:30:45 +0500

    Update Q5.c

--- Q5.c
+++ Q5.c
@@ -1,18 +1,28 @@
-#include<stdio.h>
-int main(){
-	int num[12],i,temp,rem;
-	for(i=0;i<12;i++){
-		printf("Enter number %d\n",i+1);
-		scanf("%d",&num[i]);
-	}
-	printf("Enter number you want to remove\n");
-	scanf("%d",&rem);
-	for(i=0;i<12;i++){
-		if(num[i]==rem){
-			num[i]=num[i+1];
-		}
-}
-	for(i=0;i<12;i++){
-		printf("%d	",num[i]);
-	}
+#include <stdio.h>
+
+int main() {
+    int num[12], i, rem, j;
+
+    for (i = 0; i < 12; i++) {
+        printf("Enter number %d: ", i + 1);
+        scanf("%d", &num[i]);
+    }
+
+    printf("Enter number you want to remove: ");
+    scanf("%d", &rem);
+
+    j = 0;  
+    for (i = 0; i < 12; i++) {
+        if (num[i] != rem) {
+            num[j] = num[i]; 
+            j++;
+        }
+    }
+
+    printf("Array after removing %d:\n", rem);
+    for (i = 0; i < j; i++) {
+        printf("%d\t", num[i]);
+    }
+
+    return 0;
 }
